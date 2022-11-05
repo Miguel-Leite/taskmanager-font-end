85caf8f5eea9d351761a491e12bc15eda00e88d7
Miguel-Leite <miguelleite200leite@email.com>
2022-11-06 00:33:09 +0100

    :white_check_mark: Page my tasks created

---
+++ src/pages/tasks/index.tsx
@@ -0,0 +1,13 @@
+import { ListTasks } from "../../components/ListTasks";
+import { TopBar } from "../../components/TopBar";
+
+
+export default function Tasks() {
+
+  return (
+    <div>
+      <TopBar />
+      <ListTasks />
+    </div>
+  )
+}
