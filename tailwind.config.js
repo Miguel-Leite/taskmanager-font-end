6fd747ad086f0e21068044f24335e94aab121d9c
Miguel-Leite <miguelleite200leite@email.com>
2022-11-08 18:15:13 +0100

    :recycle: add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'dark-600': '#2E2938',
         'dark-400': '#363041',
         'secundary-ws': '#B4ACF9',
+        'secondary-100': 'rgba(255, 255, 255, 0.6)',
       },
       fontFamily: {
         // sans: ['Asap, sans-sarif','Roboto, sans-sarif'],
